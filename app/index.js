9cc88a7b10376bfa7d8554b1ae9c414d8e4627d9
Kuon8660 <980478660@qq.com>
2025-02-12 10:43:41 +0800

    完善版本号，更改插件信息

--- app/index.js
+++ app/index.js
@@ -2,9 +2,8 @@ import { log } from "./log.js";
 import apptest from "./apptest.js";
 import RebirthPlugin from "./rebirth.js"; // 引入新的插件
 
+export { apptest, RebirthPlugin }; // 导出新的插件
 // 日志记录
 log("info", "------rebirth-plugin------");
 log("info", "rebirth-plugin 插件加载成功");
 log("info", "------rebirth-plugin------");
-
-export { apptest, RebirthPlugin }; // 导出新的插件
